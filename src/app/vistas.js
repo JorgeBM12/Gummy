ccd91bae642e1c8223a18d7dccf2ef6a66811211
AngelitoYT <angelsuarezhuerta987@gmail.com>
2025-12-06 11:33:36 -0600

    Cambios de estilo global

--- src/app/vistas.js
+++ src/app/vistas.js
@@ -11,11 +11,13 @@ const estrategias = {
     </ul>
   `,
   formulario: () => `
+  <link rel="stylesheet" href="../styles/global.css">
+  <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
     <h1>Nuevo Gummy</h1>
     <form method="POST" action="/guardar">
       <input name="titulo" placeholder="Título" required />
       <textarea name="descripcion" placeholder="Descripción"></textarea>
-      <button type="submit">Guardar</button>
+      <button type="submit" class="btn-verde">Guardar</button>
     </form>
   `
 };
